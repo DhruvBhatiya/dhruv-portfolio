4d52f1164022fcf4079384da72eaa60a572281b6
Dhruv <dhruvcomputers11@gmail.com>
2025-01-04 12:49:39 +0530

    Project add

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -1,6 +1,9 @@
 import { addCart, briotechno, movieApp, newsMonkey, pokemonApp, projImg1, projImg2, projImg3, youtubeClone } from "../../comman/images";
-
+// Project 
 import ghargharbazaar from '../../assets/img/project/ghargharbazaar.jpg'
+import arabinfradesign from '../../assets/img/project/arabinfradesign.jpg'
+import shekharStone from '../../assets/img/project/shekhar-stone.png'
+import dnd from '../../assets/img/project/dnd.png'
 
 export const projectData = [
 
@@ -11,6 +14,24 @@ export const projectData = [
                 description: "This project highlights my expertise in developing scalable, user-friendly e-commerce platforms, showcasing my ability to deliver quality solutions tailored to client needs.",
                 imgUrl: ghargharbazaar,
                 link: 'https://www.ghargharbazaar.com/'
+            },
+            {
+                title: "Arab Infradesign",
+                description: "A cutting-edge architecture and interior design platform dedicated to transforming ideas into stunning, functional spaces. Arabinfra Design combines creativity, innovation, and technical expertise to deliver exceptional design solutions tailored to client needs.",
+                imgUrl: arabinfradesign,
+                link: 'https://arabinfradesign.in/'
+            },
+            {
+                title: "Shekhar Stone Company",
+                description: "A premier platform showcasing exquisite stone designs and craftsmanship, Shekhar Stone is your destination for high-quality stone products that combine durability with elegance. The platform highlights a wide range of natural and engineered stones suitable for various architectural and interior design needs.",
+                imgUrl: shekharStone,
+                link: 'https://shekhar-stone.vercel.app/'
+            },
+            {
+                title: "DnD Solutions",
+                description: "DnD Solutions is a forward-thinking company dedicated to providing high-quality, innovative solutions across various industries. With a diverse product portfolio, we specialize in manufacturing.",
+                imgUrl: dnd,
+                link: 'https://www.dndsolutions.in/'
             },
             
         ],
