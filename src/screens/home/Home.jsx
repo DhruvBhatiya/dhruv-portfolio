38dbf24af3af7713eb3f66d589dc59a08e8c46d8
dhruvbriotechno <154588490+dhruvbriotechno@users.noreply.github.com>
2025-01-06 22:33:24 +0530

    validation add

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Banner } from "../../components/Banner";
-import { Contact } from "../../components/Contact";
+import { Contact } from "../../components/contact/Contact";
 import { Footer } from "../../components/Footer";
 import { NavBar } from "../../components/NavBar";
 import { Projects } from "../../components/Projects/Projects";
